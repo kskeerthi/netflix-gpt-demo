ef30067eacd1ef866eb1eaa479d73ccece243169
keerthi <sk.keetu@gmail.com>
2024-02-19 16:43:28 +0530

    login and sign up frontend validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import Header from './Header'
+import { validateEmailAndPass } from '../utils/validate';
 
 const Login = () => {
     const[issignInForm , setIsSignInForm] = useState(true);
+    const[errorMessage, setErrorMessage ] = useState("");
+    const email = useRef(null);
+    const password = useRef(null);
     const toggleSignInForm = () => {
         setIsSignInForm(!issignInForm);
+    }
+    const handleButtonClick = () => {
+        const message  = validateEmailAndPass(email.current.value, password.current.value);
+        setErrorMessage(message);
+       
     }
   return (
     <div>
@@ -12,13 +21,14 @@ const Login = () => {
         <div className='absolute'>
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/c0b69670-89a3-48ca-877f-45ba7a60c16f/2642e08e-4202-490e-8e93-aff04881ee8a/IN-en-20240212-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt="netflix-background"/>
         </div>
-        <form className='w-3/12 absolute my-36 mx-auto left-0 right-0 p-12 bg-black bg-opacity-80 text-white'>
+        <form onSubmit={(e) => e.preventDefault()} className='w-3/12 absolute my-36 mx-auto left-0 right-0 p-12 bg-black bg-opacity-80 text-white'>
             <h1 className='font-bold text-3xl py-4 text-white'> {issignInForm? 'Sign In' : 'Sign Up'}</h1>
             {!issignInForm && 
             <input type="name" placeholder='Enter your name' className='p-2 my-2 w-full bg-gray-800 rounded-lg '/>}
-            <input type="email" placeholder='Email address' className='p-2 my-2 w-full bg-gray-800 rounded-lg '/>
-            <input type="password" placeholder='Password' className='p-2 my-2 w-full bg-gray-800 rounded-lg'/>
-            <button type="submit" className='p-4 my-4 bg-red-700 rounded-lg text-white w-full'>{issignInForm? 'Sign In' : 'Sign Up'}</button>
+            <input ref={email} type="email" placeholder='Email address' className='p-2 my-2 w-full bg-gray-800 rounded-lg '/>
+            <input ref={password} type="password" placeholder='Password' className='p-2 my-2 w-full bg-gray-800 rounded-lg'/>
+            <p className='text-red-500 font-bold text-lg p-2'>{errorMessage}</p>
+            <button type="submit" className='p-4 my-4 bg-red-700 rounded-lg text-white w-full' onClick={handleButtonClick}>{issignInForm? 'Sign In' : 'Sign Up'}</button>
             <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>{issignInForm? 'New to Netflix? Sign-up here' : 'Already Registered, Sign In now'}</p>
         </form>
     </div>
---
+++ src/utils/validate.jsx
@@ -0,0 +1,11 @@
+
+
+export const validateEmailAndPass = (email, password) => {
+    const regEmail = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
+    const passValid = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(password)
+    console.log(regEmail);
+    console.log(passValid);
+    if(!regEmail) return 'Email ID is not valid';
+    if(!passValid) return 'Password is not valid';
+    return null;
+}
