1e7a60b039acb0602ace53d708cc516590d9a7f2
Keerthi koppolu <sk.keetu@gmail.com>
2024-02-20 09:24:17 +0000

    redux setup along with signin signup authentication

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useRef } from 'react'
 import Header from './Header'
 import { validateEmailAndPass } from '../utils/validate';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../utils/firebase';
+
 
 const Login = () => {
     const[issignInForm , setIsSignInForm] = useState(true);
@@ -13,6 +16,37 @@ const Login = () => {
     const handleButtonClick = () => {
         const message  = validateEmailAndPass(email.current.value, password.current.value);
         setErrorMessage(message);
+        if(message) return;
+
+        if(!issignInForm) {
+            createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+            .then((userCredential) => {
+                // Signed up 
+                const user = userCredential.user;
+                console.log(user);
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                setErrorMessage(errorCode+"-"+errorMessage);
+                // ..
+            });
+        } else {
+            signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log(user);
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                setErrorMessage(errorCode+"-"+errorMessage);
+            });
+
+        }
        
     }
   return (
