590c85a25d262884055e8a5b4393d282733b8653
keerthi <sk.keetu@gmail.com>
2024-02-20 12:57:01 +0530

    google firebase

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
   return (
     <div>
         <Header />
-        <div className='absolute'>
+        <div className='absolute '>
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/c0b69670-89a3-48ca-877f-45ba7a60c16f/2642e08e-4202-490e-8e93-aff04881ee8a/IN-en-20240212-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt="netflix-background"/>
         </div>
         <form onSubmit={(e) => e.preventDefault()} className='w-3/12 absolute my-36 mx-auto left-0 right-0 p-12 bg-black bg-opacity-80 text-white'>
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDm1WwFpK3GdurSZYLdhCCM0RvTyaaiqcs",
+  authDomain: "netflixgpt-demo.firebaseapp.com",
+  projectId: "netflixgpt-demo",
+  storageBucket: "netflixgpt-demo.appspot.com",
+  messagingSenderId: "486862043140",
+  appId: "1:486862043140:web:2ee59f8f2aa2cdf912dd51",
+  measurementId: "G-M2VJBK5ECC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
